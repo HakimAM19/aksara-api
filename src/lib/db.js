00a382e442343481570cd9eb070cb1988846565f
Hakim Al Munawar <Hakim070420@gmail.com>
2026-05-15 22:32:11 +0700

    connect project to railway database

--- src/lib/db.js
+++ src/lib/db.js
@@ -15,3 +15,12 @@ const db = mysql.createPool({
 });
 
 export default db;
+
+// import mysql from "mysql2/promise"; 
+// const db = mysql.createPool({ 
+// host: "localhost", 
+// user: "root", 
+// password: "", 
+// database: "premium_article_db", 
+// }); 
+// export default db;
