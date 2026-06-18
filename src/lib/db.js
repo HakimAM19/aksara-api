ae2870cb4e895d344b728faa8538409bd25d68c5
Hakim Al Munawar <Hakim070420@gmail.com>
2026-06-18 23:26:58 +0700

    OTW DEPLOYYY 4

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,17 +1,24 @@
+// import mysql from "mysql2/promise";
+
+// const db = mysql.createPool({
+
+//   host: process.env.DB_HOST,
+
+//   user: process.env.DB_USER,
+
+//   password: process.env.DB_PASSWORD,
+
+//   database: process.env.DB_NAME,
+
+//   port: process.env.DB_PORT,
+
+// });
+
+// export default db;
+
 import mysql from "mysql2/promise";
 
-const db = mysql.createPool({
-
-  host: process.env.DB_HOST,
-
-  user: process.env.DB_USER,
-
-  password: process.env.DB_PASSWORD,
-
-  database: process.env.DB_NAME,
-
-  port: process.env.DB_PORT,
-
-});
+// Langsung tembak pakai DATABASE_URL tunggal agar fleksibel di localhost maupun Vercel
+const db = mysql.createPool(process.env.DATABASE_URL);
 
 export default db;
