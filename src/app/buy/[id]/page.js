f6de35368d73c61c25cc33491aec763415003243
Hakim Al Munawar <Hakim070420@gmail.com>
2026-06-18 23:42:57 +0700

    OTW DEPLOYYY 5

--- src/app/buy/[id]/page.js
+++ src/app/buy/[id]/page.js
@@ -55,7 +55,7 @@ export default function BuyArticlePage({
     try {
 
       await fetch(
-        "http://localhost:3000/api/article-purchases",
+        "/api/article-purchases",
         {
 
           method: "POST",
